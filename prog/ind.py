4be5daeeac5da82b2563df33ebaffb41406d8f24
Hilol <imanuilyusupov@gmail.com>
2024-02-18 18:49:34 +0300

    update

--- prog/ind.py
+++ prog/ind.py
@@ -4,116 +4,39 @@
 # Решить индивидуальное задание лабораторной работы 2.6,
 # оформив каждую команду в виде отдельной функции.
 
-import bisect
-import re
-import sys
+# Функция для добавления записи в список и сортировки по датам рождения
+def add_person_to_list(people_list):
+    person = {}
+    print("\nВведите данные о человеке:")
+    person["фамилия"] = input("Фамилия: ")
+    person["имя"] = input("Имя: ")
+    person["знак Зодиака"] = input("Знак Зодиака: ")
+    birthday = list(map(int, input("Дата рождения (через пробел дд мм гггг): ").split()))
+    person["дата рождения"] = birthday
+    people_list.append(person)
+    people_list.sort(key=lambda x: tuple(x["дата рождения"]))
+    print("Данные добавлены и упорядочены по датам рождения.")
 
 
-def get_route():
-    """
-    Запросить данные о маршруте.
-    """
-    start = input("Введите начальный пункт: ")
-    end = input("Введите конечный пункт: ")
-    count = int(input("Введите номер маршрута: "))
-
-    return {
-        'начальный пункт': start,
-        'конечный пункт': end,
-        'номер маршрута': count
-    }
+# Функция для отображения информации о людях с определенным знаком Зодиака
+def display_people_by_zodiac_sign(people_list, search_zodiac_sign):
+    found = False
+    print(f"\nЛюди с знаком Зодиака '{search_zodiac_sign}':")
+    for person in people_list:
+        if person["знак Зодиака"] == search_zodiac_sign:
+            print(f"{person['фамилия']} {person['имя']} - {person['дата рождения'][0]}.{person['дата рождения'][1]}.{person['дата рождения'][2]}")
+            found = True
+    if not found:
+        print(f"Нет людей с знаком Зодиака '{search_zodiac_sign}'.")
 
 
-def display_routes(routes):
-    """
-    Отобразить список маршрутов.
-    """
-    if routes:
-        line = '+-{}-+-{}-+-{}-+'.format(
-            '-' * 30,
-            '-' * 20,
-            '-' * 8
-        )
-        print(line)
-        print(
-            '| {:^30} | {:^20} | {:^8} |'.format(
-                "Начало",
-                "Конец",
-                "Номер"
-            )
-        )
-        print(line)
-        for route in routes:
-            print(
-                '| {:<30} | {:<20} | {:>8} |'.format(
-                    route.get('начальный пункт', ''),
-                    route.get('конечный пункт', ''),
-                    route.get('номер маршрута', '')
-                )
-            )
-        print(line)
-    else:
-        print("Список маршрутов пуст.")
+# Создание списка людей в виде словарей
+people_data = []
 
+# Добавление данных о людях и сортировка по датам рождения
+add_person_to_list(people_data)
+add_person_to_list(people_data)
 
-def select_routes(routes, name_punct):
-    """
-    Выбрать маршруты с заданным пунктом отправления или прибытия.
-    """
-    selected = []
-    for route in routes:
-        if route['начальный пункт'].lower() == name_punct \
-                or route['конечный пункт'].lower() == name_punct:
-            selected.append(route)
-
-    return selected
-
-
-def main():
-    """
-    Главная функция программы.
-    """
-    routes = []
-    while True:
-        command = input(">>> ").lower()
-
-        match command:
-            case 'exit':
-                break
-
-            case 'add':
-                route = get_route()
-                if route not in routes:
-                    bisect.insort(
-                        routes, route, key=lambda item: item.get(
-                            'номер маршрута'
-                        )
-                    )
-                else:
-                    print("Данный маршрут уже добавлен.")
-
-            case 'list':
-                display_routes(routes)
-
-            case _ if (m := re.match(r'select (.+)', command)):
-                name_punct = m.group(1)
-                selected = select_routes(routes, name_punct)
-                display_routes(selected)
-
-            case 'help':
-                print("Список команд:\n")
-                print("add - добавить маршрут;")
-                print("list - вывести список маршрутов;")
-                print(
-                    "select <название пункта> - запросить маршруты, которые начинаются\n"
-                    "или заканчиваются в данном пункте;"
-                )
-                print("help - отобразить справку;")
-                print("exit - завершить работу с программой.")
-
-            case _:
-                print(f"Неизвестная команда {command}", file=sys.stderr)
-
-
-if __name__ == '__main__':
-    main()
+# Ввод знака Зодиака для поиска
+searched_zodiac_sign = input("\nВведите знак Зодиака для поиска: ")
+display_people_by_zodiac_sign(people_data, searched_zodiac_sign)
